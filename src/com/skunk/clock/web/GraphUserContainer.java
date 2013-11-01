08aecf9069242b555370d180fc1e4e36c696cc2f
Westin Miller <equinoxscripts@gmail.com>
2013-10-31 18:59:09 -0700

    Added fancy date picker stuff.
    Also user ordering.

--- src/com/skunk/clock/web/GraphUserContainer.java
+++ src/com/skunk/clock/web/GraphUserContainer.java
@@ -34,77 +34,91 @@ public class GraphUserContainer implements Container {
 			response.setDate("Last-Modified", time);
 
 			body.println("<html><head>");
-			body.println("<link rel=\"stylesheet\" href=\"/data/resources/chart.css\" type=\"text/css\" media=\"screen\">");
-			body.println("<meta charset=\"utf-8\"><script src=\"/data/resources/raphael.js\"></script><script src=\"/data/resources/popup.js\"></script><script src=\"/data/resources/jquery.js\"></script><script src=\"/data/resources/user_graph.js\"></script>");
-			body.println("</head><body>");
 
 			Map<String, String> data = WebUtil.decodeGET(request.getTarget());
 			String start = data.get("start");
 			String end = data.get("end");
 			String uuid = data.get("uuid");
 
-			if (start == null && end != null) {
-				start = String.valueOf(Long.valueOf(end) - DEFAULT_LENGTH);
-			} else if (start != null && end == null) {
-				end = String.valueOf(Long.valueOf(start) + DEFAULT_LENGTH);
-			} else if (start == null && end == null) {
-				Calendar today = Calendar.getInstance();
-				today.set(Calendar.getInstance().get(Calendar.YEAR), Calendar
-						.getInstance().get(Calendar.MONTH), Calendar
-						.getInstance().get(Calendar.DAY_OF_MONTH), 0, 0, 0);
-				start = String
-						.valueOf(today.getTimeInMillis() - DEFAULT_LENGTH);
-				end = String.valueOf(today.getTimeInMillis()
-						+ (24 * 60 * 60 * 1000) - 1);
-			}
-			if (start != null && end != null && uuid != null) {
-				final long startTime = Long.valueOf(start);
-				final long endTime = Long.valueOf(end) < 0 ? Long.MAX_VALUE
-						: Long.valueOf(end);
-				Member mem = server.getDatabase().getMembers()
-						.getMemberByBadge(uuid);
-				if (mem == null) {
-					try {
-						mem = server.getDatabase().getMembers()
-								.getMemberByUUID(Long.valueOf(uuid));
-					} catch (Exception e) {
-					}
-				}
-				File[] list = WebUtil.listOrderedDatabase(startTime, endTime);
-				long[][] timeHeaders = new long[list.length][2];
-				int i = 0;
-				for (File f : list) {
-					ClocktimeDatabase db = server.getDatabase().getClocktime(
-							f.getAbsolutePath());
-					Clocktime info = db.getClocktime(mem);
-					if (info != null) {
-						timeHeaders[i][0] = db.getCreation();
-						timeHeaders[i][1] = info.getClockTime();
-						i++;
-					} else {
-						timeHeaders[i][0] = Long.MAX_VALUE;
-						i++;
-					}
-				}
-
-				body.println("<table id=data>");
-				body.println("<tfoot><tr>");
-				for (int j = 0; j < i; j++) {
-					body.println("<th>" + timeHeaders[j][0] + "</th>");
-				}
-				body.println("</tr></tfoot>");
-				body.println("<tbody><tr>");
-				for (int j = 0; j < i; j++) {
-					body.println("<td>" + timeHeaders[j][1] + "</td>");
-				}
-				body.println("</tr></tbody>");
-				body.println("</table>");
-				body.println("<div id='title'>" + mem.getName() + "</div>");
-				body.println("<div id='holder'></div>");
+			if (uuid == null) {
+				body.println("<link rel=\"stylesheet\" href=\"/data/resources/datepickr.css\" type=\"text/css\" media=\"screen\">");
+				body.println("<meta charset=\"utf-8\"><script src=\"/data/resources/datepickr.min.js\"></script>");
+				body.println("</head><body>");
+				body.println("<form method='GET' action=''>");
+				body.println("User: <input type='text' name='uuid'/></br>");
+				body.println("Start: <input type='text' name='start' id='dateEnterA'/><span style='padding: 0px 25px;'>|</span>");
+				body.println("End: <input type='text' name='end' id='dateEnterB'/><span style='padding: 0px 25px;'>|</span>");
+				body.println("<input type='submit'/></form>");
+				body.println("<script type='text/javascript'>new datepickr('dateEnterB', {'dateFormat': 's'});new datepickr('dateEnterA', {'dateFormat': 's'});</script>");
 			} else {
-				body.println("Requires a start and end key!");
-			}
+				body.println("<link rel=\"stylesheet\" href=\"/data/resources/chart.css\" type=\"text/css\" media=\"screen\">");
+				body.println("<meta charset=\"utf-8\"><script src=\"/data/resources/raphael.js\"></script><script src=\"/data/resources/popup.js\"></script><script src=\"/data/resources/jquery.js\"></script><script src=\"/data/resources/user_graph.js\"></script>");
+				body.println("</head><body>");
 
+				if (start == null && end != null) {
+					start = String.valueOf(Long.valueOf(end) - DEFAULT_LENGTH);
+				} else if (start != null && end == null) {
+					end = String.valueOf(Long.valueOf(start) + DEFAULT_LENGTH);
+				} else if (start == null && end == null) {
+					Calendar today = Calendar.getInstance();
+					today.set(Calendar.getInstance().get(Calendar.YEAR),
+							Calendar.getInstance().get(Calendar.MONTH),
+							Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+							0, 0, 0);
+					start = String.valueOf(today.getTimeInMillis()
+							- DEFAULT_LENGTH);
+					end = String.valueOf(today.getTimeInMillis()
+							+ (24 * 60 * 60 * 1000) - 1);
+				}
+				if (start != null && end != null && uuid != null) {
+					final long startTime = Long.valueOf(start);
+					final long endTime = Long.valueOf(end) < 0 ? Long.MAX_VALUE
+							: Long.valueOf(end);
+					Member mem = server.getDatabase().getMembers()
+							.getMemberByBadge(uuid);
+					if (mem == null) {
+						try {
+							mem = server.getDatabase().getMembers()
+									.getMemberByUUID(Long.valueOf(uuid));
+						} catch (Exception e) {
+						}
+					}
+					File[] list = WebUtil.listOrderedDatabase(startTime,
+							endTime);
+					long[][] timeHeaders = new long[list.length][2];
+					int i = 0;
+					for (File f : list) {
+						ClocktimeDatabase db = server.getDatabase()
+								.getClocktime(f.getAbsolutePath());
+						Clocktime info = db.getClocktime(mem);
+						if (info != null) {
+							timeHeaders[i][0] = db.getCreation();
+							timeHeaders[i][1] = info.getClockTime();
+							i++;
+						} else {
+							timeHeaders[i][0] = Long.MAX_VALUE;
+							i++;
+						}
+					}
+
+					body.println("<table id=data>");
+					body.println("<tfoot><tr>");
+					for (int j = 0; j < i; j++) {
+						body.println("<th>" + timeHeaders[j][0] + "</th>");
+					}
+					body.println("</tr></tfoot>");
+					body.println("<tbody><tr>");
+					for (int j = 0; j < i; j++) {
+						body.println("<td>" + timeHeaders[j][1] + "</td>");
+					}
+					body.println("</tr></tbody>");
+					body.println("</table>");
+					body.println("<div id='title'>" + mem.getName() + "</div>");
+					body.println("<div id='holder'></div>");
+				} else {
+					body.println("Requires a start and end key!");
+				}
+			}
 			body.println("</body></html>");
 			body.close();
 		} catch (Exception e) {
--- src/com/skunk/clock/web/VisualizePeriodContainer.java
+++ src/com/skunk/clock/web/VisualizePeriodContainer.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.PrintStream;
 import java.util.Calendar;
-import java.util.HashMap;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
@@ -15,6 +17,7 @@ import org.simpleframework.http.core.Container;
 import com.skunk.clock.db.Clocktime;
 import com.skunk.clock.db.ClocktimeDatabase;
 import com.skunk.clock.db.Member;
+import com.skunk.clock.db.Member.MemberType;
 
 public class VisualizePeriodContainer implements Container {
 	private ClockWebServer server;
@@ -41,6 +44,7 @@ public class VisualizePeriodContainer implements Container {
 			Map<String, String> data = WebUtil.decodeGET(request.getTarget());
 			String start = data.get("start");
 			String end = data.get("end");
+			boolean flaggy = false;
 			if (start == null && end != null) {
 				start = String.valueOf(Long.valueOf(end) - DEFAULT_LENGTH);
 			} else if (start != null && end == null) {
@@ -54,73 +58,108 @@ public class VisualizePeriodContainer implements Container {
 						.valueOf(today.getTimeInMillis() - DEFAULT_LENGTH);
 				end = String.valueOf(today.getTimeInMillis()
 						+ (24 * 60 * 60 * 1000) - 1);
+				flaggy = true;
 			}
-			if (start != null && end != null) {
-				final long startTime = Long.valueOf(start);
-				final long endTime = Long.valueOf(end) < 0 ? Long.MAX_VALUE
-						: Long.valueOf(end);
-				File[] list = new File("./data")
-						.listFiles(new FilenameFilter() {
-							@Override
-							public boolean accept(File dir, String name) {
-								if (name.startsWith("time_chunk_")) {
-									// Parse as a date
-									String[] dateInfo = name.substring(11,
-											name.length() - 4).split("_");
-									if (dateInfo.length == 3) {
-										Calendar cal = Calendar.getInstance();
-										cal.set(Integer.valueOf(dateInfo[2]),
-												Integer.valueOf(dateInfo[1]) - 1,
-												Integer.valueOf(dateInfo[0]),
-												0, 0, 0);
-										if (cal.getTimeInMillis() >= startTime
-												&& cal.getTimeInMillis() <= endTime) {
-											return true;
+			if (flaggy) {
+				body.println("<link rel=\"stylesheet\" href=\"/data/resources/datepickr.css\" type=\"text/css\" media=\"screen\">");
+				body.println("<meta charset=\"utf-8\"><script src=\"/data/resources/datepickr.min.js\"></script>");
+				body.println("</head><body>");
+				body.println("<form method='GET' action=''>");
+				body.println("Start: <input type='text' name='start' id='dateEnterA' value='"
+						+ start
+						+ "'/><span style='padding: 0px 25px;'>|</span>");
+				body.println("End: <input type='text' name='end' id='dateEnterB' value='"
+						+ end + "'/><span style='padding: 0px 25px;'>|</span>");
+				body.println("<input type='submit'/></form>");
+				body.println("<script type='text/javascript'>new datepickr('dateEnterB', {'dateFormat': 's'});new datepickr('dateEnterA', {'dateFormat': 's'});</script>");
+			} else {
+				if (start != null && end != null) {
+					final long startTime = Long.valueOf(start);
+					final long endTime = Long.valueOf(end) < 0 ? Long.MAX_VALUE
+							: Long.valueOf(end);
+					File[] list = new File("./data")
+							.listFiles(new FilenameFilter() {
+								@Override
+								public boolean accept(File dir, String name) {
+									if (name.startsWith("time_chunk_")) {
+										// Parse as a date
+										String[] dateInfo = name.substring(11,
+												name.length() - 4).split("_");
+										if (dateInfo.length == 3) {
+											Calendar cal = Calendar
+													.getInstance();
+											cal.set(Integer
+													.valueOf(dateInfo[2]),
+													Integer.valueOf(dateInfo[1]) - 1,
+													Integer.valueOf(dateInfo[0]),
+													0, 0, 0);
+											if (cal.getTimeInMillis() >= startTime
+													&& cal.getTimeInMillis() <= endTime) {
+												return true;
+											}
 										}
 									}
+									return false;
 								}
-								return false;
+							});
+					Map<Member, Clocktime> totalData = new TreeMap<Member, Clocktime>(
+							new Comparator<Member>() {
+								@Override
+								public int compare(Member o1, Member o2) {
+									return o1.getReversedName()
+											.compareToIgnoreCase(
+													o2.getReversedName());
+								}
+							});
+					MemberType[] types = new MemberType[] { MemberType.STUDENT };
+					if (data.containsKey("mentor")) {
+						types = new MemberType[] { MemberType.MENTOR,
+								MemberType.COACH };
+					} else if (data.containsKey("all")) {
+						types = new MemberType[0];
+					}
+					for (File f : list) {
+						ClocktimeDatabase db = server.getDatabase()
+								.getClocktime(f.getAbsolutePath());
+						List<Entry<Member, Clocktime>> lst = db.getListByType(types);
+						for (Entry<Member, Clocktime> entry : lst) {
+							if (entry.getValue().getClockTime() <= 0
+									&& !data.containsKey("all")) {
+								continue;
 							}
-						});
-				Map<Member, Clocktime> totalData = new HashMap<Member, Clocktime>();
-				for (File f : list) {
-					ClocktimeDatabase db = server.getDatabase().getClocktime(
-							f.getAbsolutePath());
-					for (Entry<Member, Clocktime> entry : db.getListByType()) {
-						if (entry.getValue().getClockTime() <= 0
-								&& !data.containsKey("all")) {
-							continue;
+							Clocktime supp = totalData.get(entry.getKey());
+							if (supp == null) {
+								supp = new Clocktime();
+								totalData.put(entry.getKey(), supp);
+							}
+							supp.insertClockTime(entry.getValue());
 						}
-						Clocktime supp = totalData.get(entry.getKey());
-						if (supp == null) {
-							supp = new Clocktime();
-							totalData.put(entry.getKey(), supp);
+					}
+					body.println("From " + WebUtil.formatDate(startTime)
+							+ " to " + WebUtil.formatDate(endTime) + "</br>");
+					if (totalData.size() != 0) {
+						long totalTime = (long) (1000 * 60 * 60 * 2.5f)
+								* list.length;
+						body.println("Days: " + list.length + "</br>");
+						body.println("<table border=1>");
+						body.println("<tr><th>User</th><th>Time</th><th>Percentage</th></tr>");
+						for (Entry<Member, Clocktime> entry : totalData
+								.entrySet()) {
+							body.println("<tr><td>"
+									+ entry.getKey().getName()
+									+ "</td><td>"
+									+ WebUtil.formatTimeLong(entry.getValue()
+											.getClockTime())
+									+ "</td><td>"
+									+ WebUtil.formatPercentage(entry.getValue()
+											.getClockTime(), totalTime)
+									+ "</td></tr>");
 						}
-						supp.insertClockTime(entry.getValue());
+						body.println("</table>");
 					}
+				} else {
+					body.println("Requires a start and end key!");
 				}
-				body.println("From " + WebUtil.formatDate(startTime) + " to "
-						+ WebUtil.formatDate(endTime) + "</br>");
-				if (totalData.size() != 0) {
-					long totalTime = (long) (1000 * 60 * 60 * 2.5f)
-							* list.length;
-					body.println("Days: " + list.length + "</br>");
-					body.println("<table border=1>");
-					body.println("<tr><th>User</th><th>Time</th><th>Percentage</th></tr>");
-					for (Entry<Member, Clocktime> entry : totalData.entrySet()) {
-						body.println("<tr><td>"
-								+ entry.getKey().getName()
-								+ "</td><td>"
-								+ WebUtil.formatTimeLong(entry.getValue()
-										.getClockTime())
-								+ "</td><td>"
-								+ WebUtil.formatPercentage(entry.getValue().getClockTime(),
-										totalTime) + "</td></tr>");
-					}
-					body.println("</table>");
-				}
-			} else {
-				body.println("Requires a start and end key!");
 			}
 
 			body.println("</body></html>");
