da87322cbd9c195eeeea47e4f1dcf4b0ca87d098
Westin Miller <equinoxscripts@gmail.com>
2013-12-17 18:04:41 -0800

    Fixed bugs in the user visualization.  - Also made black magic happen when somebody goofs. (A user)  - Made it use the common code.

--- src/com/skunk/clock/web/VisualizeUserContainer.java
+++ src/com/skunk/clock/web/VisualizeUserContainer.java
@@ -40,7 +40,7 @@ public class VisualizeUserContainer extends APIHandler {
 			body.println("<script type='text/javascript'>new datepickr('dateEnterB', {'dateFormat': 's'});new datepickr('dateEnterA', {'dateFormat': 's'});</script>");
 		} else {
 			body.println("<html><body>");
-			
+
 			if (start == null && end != null) {
 				start = String.valueOf(Long.valueOf(end) - DEFAULT_LENGTH);
 			} else if (start != null && end == null) {
@@ -68,82 +68,87 @@ public class VisualizeUserContainer extends APIHandler {
 					} catch (Exception e) {
 					}
 				}
-				File[] list = WebUtil.listOrderedDatabase(startTime, endTime);
-				long[][] timeHeaders = new long[list.length][2];
-				ClocktimeDatabase[] mapping = new ClocktimeDatabase[list.length];
-				int i = 0;
-				for (File f : list) {
-					ClocktimeDatabase db = server.getDatabase().getClocktime(
-							f.getAbsolutePath());
-					Clocktime info = db.getClocktime(mem);
-					if (info != null) {
-						timeHeaders[i][0] = db.getCreation();
-						timeHeaders[i][1] = info.getClockTime();
-						mapping[i] = db;
-						i++;
-					} else {
-						timeHeaders[i][0] = db.getCreation();
-						timeHeaders[i][1] = 0;
-						mapping[i] = db;
-						i++;
-					}
-				}
-
-				body.println("<div style='float: left;'>");
-				body.println("<img src='/visual/user/image?uuid=" + uuid
-						+ "'/>");
-				body.println("</div>");
-
-				body.println("<b>" + mem.getName() + "</b></br>");
-				if (mem.getGroups().length > 0
-						&& mem.getGroups()[0] != MemberGroup.UNDEFINED) {
-					body.print("In groups: ");
-					for (int j = 0; j < mem.getGroups().length; j++) {
-						if (j == mem.getGroups().length - 1) {
-							if (mem.getGroups().length == 2) {
-								body.print(" and ");
-							} else if (mem.getGroups().length > 2) {
-								body.print(", and ");
-							}
-						} else if (j > 0) {
-							body.print(", ");
+				if (mem != null) {
+					File[] list = WebUtil.listOrderedDatabase(startTime,
+							endTime);
+					long[][] timeHeaders = new long[list.length][2];
+					ClocktimeDatabase[] mapping = new ClocktimeDatabase[list.length];
+					int i = 0;
+					for (File f : list) {
+						ClocktimeDatabase db = server.getDatabase()
+								.getClocktime(f.getAbsolutePath());
+						Clocktime info = db.getClocktime(mem);
+						if (info != null) {
+							timeHeaders[i][0] = db.getCreation();
+							timeHeaders[i][1] = info.getClockTime();
+							mapping[i] = db;
+							i++;
+						} else {
+							timeHeaders[i][0] = db.getCreation();
+							timeHeaders[i][1] = 0;
+							mapping[i] = db;
+							i++;
 						}
-						body.print(mem.getGroups()[j].formattedName());
 					}
-					body.println("<br>");
-				}
-				body.println("Type: " + mem.getType().formattedName() + "</br>");
 
-				long totalTime = 0;
-				long requiredTotal = 0;
-				for (int j = 0; j < i; j++) {
-					requiredTotal += mapping[j].getRequiredTime();
-					totalTime += timeHeaders[j][1];
-				}
-				body.println("Graph User Time: <a href='/graph/user?start="
-						+ startTime + "&end=" + endTime + "&uuid="
-						+ mem.getUUID() + "'>Here</a></br>");
-				body.println("Total time: " + WebUtil.formatTimeLong(totalTime)
-						+ "</br>");
-				body.println("Percentage: "
-						+ WebUtil.formatPercentage(totalTime, requiredTotal)
-						+ "</br>");
+					body.println("<div style='float: left;'>");
+					body.println("<img src='/visual/user/image?uuid=" + uuid
+							+ "'/>");
+					body.println("</div>");
 
-				body.println("<table border=1>");
-				body.println("<tr><th>Day</th><th>Time</th><th>Percentage</th></tr>");
-				for (int j = 0; j < i; j++) {
-					body.println("<tr><td>"
-							+ WebUtil.formatDate(timeHeaders[j][0])
-							+ (mapping[j].getTag() != null ? " "
-									+ mapping[j].getTag() : "")
-							+ "</td><td>"
-							+ WebUtil.formatTimeLong(timeHeaders[j][1])
-							+ "</td><td>"
-							+ WebUtil.formatPercentage(timeHeaders[j][1],
-									mapping[j].getRequiredTime())
-							+ "</td></tr>");
+					body.println("<b>" + mem.getName() + "</b></br>");
+					if (mem.getGroups().length > 0
+							&& mem.getGroups()[0] != MemberGroup.UNDEFINED) {
+						body.print("In groups: ");
+						for (int j = 0; j < mem.getGroups().length; j++) {
+							if (j == mem.getGroups().length - 1) {
+								if (mem.getGroups().length == 2) {
+									body.print(" and ");
+								} else if (mem.getGroups().length > 2) {
+									body.print(", and ");
+								}
+							} else if (j > 0) {
+								body.print(", ");
+							}
+							body.print(mem.getGroups()[j].formattedName());
+						}
+						body.println("<br>");
+					}
+					body.println("Type: " + mem.getType().formattedName()
+							+ "</br>");
+
+					long totalTime = 0;
+					long requiredTotal = 0;
+					for (int j = 0; j < i; j++) {
+						requiredTotal += mapping[j].getRequiredTime();
+						totalTime += timeHeaders[j][1];
+					}
+					body.println("Graph User Time: <a href='/graph/user?start="
+							+ startTime + "&end=" + endTime + "&uuid="
+							+ mem.getUUID() + "'>Here</a></br>");
+					body.println("Total time: "
+							+ WebUtil.formatTimeLong(totalTime) + "</br>");
+					body.println("Percentage: "
+							+ WebUtil
+									.formatPercentage(totalTime, requiredTotal)
+							+ "</br>");
+
+					body.println("<table border=1>");
+					body.println("<tr><th>Day</th><th>Time</th><th>Percentage</th></tr>");
+					for (int j = 0; j < i; j++) {
+						body.println("<tr><td>"
+								+ WebUtil.formatDate(timeHeaders[j][0])
+								+ (mapping[j].getTag() != null ? " "
+										+ mapping[j].getTag() : "")
+								+ "</td><td>"
+								+ WebUtil.formatTimeLong(timeHeaders[j][1])
+								+ "</td><td>"
+								+ WebUtil.formatPercentage(timeHeaders[j][1],
+										mapping[j].getRequiredTime())
+								+ "</td></tr>");
+					}
+					body.println("</table>");
 				}
-				body.println("</table>");
 			} else {
 				body.println("Requires a start and end key!");
 			}
